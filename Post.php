1da5cc4ac935a684ec3b45dd5fc95583c09ce30f
ajitkrmishra <84868400+ajitkrmishra@users.noreply.github.com>
2024-05-30 16:27:45 +0530

    Create Post.php

---
+++ Post.php
@@ -0,0 +1,50 @@
+class Post
+{
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function create($title, $content)
+    {
+        // Insert the post into the database
+        $stmt = $this->db->prepare('INSERT INTO posts (title, content, created_at) VALUES (:title, :content, NOW())');
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->execute();
+
+        // Return the ID of the new post
+        return $this->db->lastInsertId();
+    }
+
+    public function read($id)
+    {
+        // Select the post from the database
+        $stmt = $this->db->prepare('SELECT * FROM posts WHERE id = :id LIMIT 1');
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $post = $stmt->fetch();
+
+        return $post;
+    }
+
+    public function update($id, $title, $content)
+    {
+        // Update the post in the database
+        $stmt = $this->db->prepare('UPDATE posts SET title = :title, content = :content WHERE id = :id');
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->execute();
+    }
+
+    public function delete($id)
+    {
+        // Delete the post from the database
+        $stmt = $this->db->prepare('DELETE FROM posts WHERE id = :id');
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+}
